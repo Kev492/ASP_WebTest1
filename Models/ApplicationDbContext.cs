56156555de2af678f590c403cdef3d5e89d47332
Kev492 <kyj12342000@gmail.com>
2023-11-19 18:08:39 +0900

    first commit

---
+++ Models/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace AspWebTest2.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Customer> CUSTOMER { get; set; }
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,10 @@
+﻿namespace AspWebTest2.Models
+{
+    public class Customer
+    {
+        public int CustomerNumber { get; set; }
+        public string CustomerID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
